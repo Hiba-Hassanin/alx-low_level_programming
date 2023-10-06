4428d57e4a4b2976ce2bbd8f7b26c635a12f1a23
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-05 18:13:08 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * class - main
+ *
+ * Description: a C program that Use the function puts
+ *
+ * Return: Alawys 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
