2ab6d4f49d6ea8f2bba9a49d1806f0c7e23b864c
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-08 17:11:40 -0700

    rotten "e" and "q". eq :D

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Description: a program that prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
