bfdc79a748fa44e26a4ba8406d53cbd41633451a
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-08 18:39:20 -0700

    separated by ,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all possible combinations of single-digit numbers
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + 0);
+
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		num++;
+
+		return (0);
+}
