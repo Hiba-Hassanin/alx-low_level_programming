b53e99a3f452bdd39e0abe2e0a663fb04258442b
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-10 20:01:22 -0700

    only abs of values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * int _abs - print the absolute value of an integer
+ *
+ * @n: the integer type for the function
+ *
+ * Return: Always 0 (success)
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
