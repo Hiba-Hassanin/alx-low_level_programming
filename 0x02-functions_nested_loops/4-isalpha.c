ed9c2a924292e9f2ef3cdfa5fb99d08273cf6574
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-10 19:41:06 -0700

    check for alphabet, both lowercase and uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: uses other function
+ *
+ * Return: if true return 1 = c, if not 0
+*/
+
+int _isalpha(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
