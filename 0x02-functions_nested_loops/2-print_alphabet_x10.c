339f5ca85bd995f17466960c179678010352935a
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-10 19:17:39 -0700

    a to z 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int diez, car;
+
+	for (diez = 0; diez <= 9; diez++)
+	{
+		for (car = 'a'; car <= 'z'; car++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
