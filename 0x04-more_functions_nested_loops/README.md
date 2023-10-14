2c17542310e523da094f2dd381262dc1bd541d94
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-13 18:58:23 -0700

    Smile

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+"We may not have hit the peak yet, but we are on the journey up." —Brian Nhira
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: print that character
+ *
+ * Return: if successful return 1, if an error is found return -1
+*/
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
