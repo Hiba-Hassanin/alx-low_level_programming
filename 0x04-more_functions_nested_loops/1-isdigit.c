13f7ae4bcaf60f6d12abf41fec66349413803c1e
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-13 19:24:09 -0700

    mul

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit (0 through 9)
+ * _isdigit - checks for a digit from 0 to 9
  *
  * @c: our variable
  *
@@ -13,5 +13,5 @@ int _isdigit(int c)
 	if (c >= 48 && c <= 57)
 		return (1);
 	else
-		return (0)
+		return (0);
 }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: our first variable
+ * @b: our second variable
+ *
+ * Return: the proudct
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
