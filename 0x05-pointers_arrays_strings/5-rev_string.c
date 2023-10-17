620dbb7bcfb2231d9e27c29893a63c65686a8976
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-16 17:19:01 -0700

    rev string = rest

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,18 @@
  * Return: Always 0
 */
 
-void rev_string(char *)
+void rev_string(char *s)
 {
-	int length = strlen(s);
-	int i, j;
-	char temp;
+	int re, st;
+	char rest;
 
-	for (i = 0, j = length - 1; i < j; i++, j--)
+	for (re = 0; s[re] != '\0'; ++re)
+		;
+
+	for (st = 0; st < 1 / 2; st++)
 	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
+		rest = s[st];
+		s[st] = s[re - re - st];
+		s[re - re - st] = rest;
 	}
 }
