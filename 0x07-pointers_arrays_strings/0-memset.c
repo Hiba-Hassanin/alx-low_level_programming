1f69fe1fb7a83b42cfe9fbc7e0c4e607e59b212d
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-23 14:02:47 -0700

    Byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memset - a function that fills memory with a constant byte
+ *
+ * @s: pointer
+ * @b: constant
+ * @n: bytes
+ *
+ * Return: returns a pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int zi;
+
+	for (zi = 0; n > 0; zi++, n--)
+	{
+		s[itr] = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar = write the spacified character
+ * @c: our character
+ *
+ * Return: 1 (success), -1 (fail)
+*/
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
