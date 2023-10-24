9707bfa9f587c0dc5a59cbf1f1c1b4331a3bc94b
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-23 20:38:39 -0700

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,7 +3,7 @@
 /**
  * *_strstr - a function that locates a substring
  *
- * @haystack: variable 
+ * @haystack: variable
  * @needle: variable
  *
  * Return: a pointer to the beginning of the located substring
@@ -11,4 +11,27 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int na;
 
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		na = 0;
+
+		if (haystack[na] == needle[na])
+		{
+			do {
+				if (needle[na + 1] == '\0')
+					return (haystack);
+
+				na++;
+
+			} while (haystack[na] == needle[na]);
+		}
+		haystack++;
+	}
+
+	return ('\0');
+}
