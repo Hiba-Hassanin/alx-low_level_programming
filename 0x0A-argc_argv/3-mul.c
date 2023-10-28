2a1a48e6bf96f7201fce225ad55517c9f70abe14
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-27 19:30:33 -0700

    tres

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers
+ *
+ * @argc: var1
+ * @argv: pointer
+ *
+ * Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+
+	return (0);
+}
