25091952b18720697e3ab7f6eceb878e9e000c52
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-10-27 18:37:29 -0700

    zero

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints a program name
+ *
+ * @argc: int
+ * @argv: the list
+ *
+ * Return: Always 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
