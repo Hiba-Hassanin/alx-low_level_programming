f61f6d288bb750f5796159a0703d3d35d7b2152e
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-01 01:59:47 +0200

    2 strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * *str_concat - a function that concatenates two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: NULL on failure
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int l1 = 0;
+	int l2 = 0;
+	char *conc;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[l1])
+		l1++;
+	while (s2[l2])
+		l2++;
+
+	conc = malloc(l1 + l2 + 1);
+	if (conc == NULL)
+		return NULL;
+
+	for (int i = 0; i < l1; i++)
+		conc[i] = s1[i];
+
+	for (int j = 0; j < l2; j++)
+		conc[l1 + j] = s2[j];
+
+	conc[l1 + l2] = '\0';
+
+	return conc;
+}
