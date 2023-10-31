406ef404e933ff2e3076deb93cc56e7a2ddb81a2
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-01 01:37:47 +0200

    check null

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,12 +11,15 @@
 
 char *_strdup(char *str)
 {
+    size_t length;
+    char *duplicate;
+
     if (str == NULL)
         return (NULL);
 
-    size_t length = strlen(str);
+    length = strlen(str);
+    duplicate = malloc((length + 1) * sizeof(char));
 
-    char *duplicate = malloc((length + 1) * sizeof(char));
     if (duplicate == NULL)
         return (NULL);
 
