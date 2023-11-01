1f0dfddb98ea9d71fb3843532b41b5d4edd2cd2b
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-01 02:51:40 +0200

    sentence to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,49 +12,60 @@
 #include <stdlib.h>
 #include <string.h>
 
-char **strtow(char *str) {
+char **strtow(char *str)
+{
 	int i, j, word_count = 0;
 	char **words = NULL;
 
-	if (str == NULL || *str == '\0') {
-		return NULL;
+	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
 	}
 
 	for (i = 0; str[i] != '\0'; i++) {
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' ')) {
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
 			word_count++;
 		}
 	}
 
-	if (word_count == 0) {
-		return NULL;
+	if (word_count == 0)
+	{
+		return (NULL);
 	}
 
 	words = (char **)malloc((word_count + 1) * sizeof(char *));
-	if (words == NULL) {
-		return NULL;
+	if (words == NULL)
+	{
+		return (NULL);
 	}
 
 	i = 0;
-	for (j = 0; str[j] != '\0'; j++) {
-		if (str[j] != ' ') {
-			if (j == 0 || str[j - 1] == ' ') {
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		if (str[j] != ' ')
+		{
+			if (j == 0 || str[j - 1] == ' ')
+			{
 				int word_length = 0;
 				int start_index = j;
 				int n;
 
-				while (str[j] != '\0' && str[j] != ' ') {
+				while (str[j] != '\0' && str[j] != ' ')
+				{
 					word_length++;
 					j++;
 				}
 
 				words[i] = (char *)malloc((word_length + 1) * sizeof(char));
-				if (words[i] == NULL) {
-					for (n = 0; n < i; n++) {
+				if (words[i] == NULL)
+				{
+					for (n = 0; n < i; n++)
+					{
 						free(words[n]);
 					}
 					free(words);
-					return NULL;
+					return (NULL);
 				}
 
 				strncpy(words[i], &str[start_index], word_length);
@@ -66,5 +77,5 @@ char **strtow(char *str) {
 
 	words[word_count] = NULL;
 
-	return words;
+	return (words);
 }
