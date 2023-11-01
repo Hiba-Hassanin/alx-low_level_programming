a8a113d69818f1a51e2aa5bbc738a68c02ec4d18
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-01 02:24:32 +0200

    remove

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * free_grid - frees 2 dimensional grid
+ *             previously created alloc_grid
+ *
+ * @**grid: the grid
+ * @height: the height
+ *
+ * Return: Nothing
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
