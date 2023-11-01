66fdf731a0ffd20fca15beef1fa6e8d82d1d6f77
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-01 02:57:18 +0200

    sentence to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,79 +3,31 @@
 
 /**
  * **strtow - splits a string into words
- *
  * @str: the string
- *
  * Return: NULL if str == NULL or str == ""
 */
 
-#include <stdlib.h>
-#include <string.h>
-
-char **strtow(char *str)
-{
-	int i, j, word_count = 0;
-	char **words = NULL;
-
-	if (str == NULL || *str == '\0')
-	{
+char **strtow(char *str) {
+	if (!str || !*str)
 		return (NULL);
-	}
 
-	for (i = 0; str[i] != '\0'; i++) {
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-		{
-			word_count++;
+	int i, j, count = 0;
+	for (i = 0; str[i]; i++)
+		count += (str[i] != ' ' && (i == 0 || str[i - 1] == ' '));
+
+	char **words = malloc((count + 1) * sizeof(char *));
+	if (!words)
+		return NULL;
+
+	for (i = 0, j = 0; str[j]; j++) {
+		if (str[j] != ' ' && (j == 0 || str[j - 1] == ' ')) {
+			int len = strcspn(&str[j], " ");
+			words[i] = malloc((len + 1) * sizeof(char));
+			strncpy(words[i++], &str[j], len);
+			j += len - 1;
 		}
 	}
 
-	if (word_count == 0)
-	{
-		return (NULL);
-	}
-
-	words = (char **)malloc((word_count + 1) * sizeof(char *));
-	if (words == NULL)
-	{
-		return (NULL);
-	}
-
-	i = 0;
-	for (j = 0; str[j] != '\0'; j++)
-	{
-		if (str[j] != ' ')
-		{
-			if (j == 0 || str[j - 1] == ' ')
-			{
-				int word_length = 0;
-				int start_index = j;
-				int n;
-
-				while (str[j] != '\0' && str[j] != ' ')
-				{
-					word_length++;
-					j++;
-				}
-
-				words[i] = (char *)malloc((word_length + 1) * sizeof(char));
-				if (words[i] == NULL)
-				{
-					for (n = 0; n < i; n++)
-					{
-						free(words[n]);
-					}
-					free(words);
-					return (NULL);
-				}
-
-				strncpy(words[i], &str[start_index], word_length);
-				words[i][word_length] = '\0';
-				i++;
-			}
-		}
-	}
-
-	words[word_count] = NULL;
-
-	return (words);
+	words[count] = NULL;
+	return words;
 }
