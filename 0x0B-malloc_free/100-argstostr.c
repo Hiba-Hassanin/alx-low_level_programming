9e95d8a331a02a4555e012979439c30886cc7636
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-01 02:34:49 +0200

    all arg

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,22 +9,28 @@
  *
  * Return: NULL if ac == 0 or av == NULL
 */
+#include <stdlib.h>
+#include <string.h>
 
 char *argstostr(int ac, char **av)
 {
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	int i, len;
+	int i;
+	int len;
 	int total_len = ac;
+	char *str;
+	char *result;
+
 	for (i = 0; i < ac; i++)
 		total_len += strlen(av[i]);
 
-	char *str = (char *)malloc(total_len + 1);
+	str = (char *)malloc(total_len + 1);
 	if (str == NULL)
 		return (NULL);
 
-	char *result = str;
+	result = str;
 	for (i = 0; i < ac; i++)
 	{
 		len = strlen(av[i]);
