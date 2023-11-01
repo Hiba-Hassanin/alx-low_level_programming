9e75c3e6729d04ffeb622073783d43834fae6bd9
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-01 02:43:04 +0200

    sentence to words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * **strtow - splits a string into words
+ *
+ * @str: the string
+ *
+ * Return: NULL if str == NULL or str == ""
+*/
+
+char **strtow(char *str) {
+	if (str == NULL || *str == '\0') return NULL;
+
+	int i, j, k, word_count = 0;
+	char **words = NULL;
+
+	for (i = 0; str[i] != '\0'; i++) {
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' ')) word_count++;
+	}
+
+	words = (char **)malloc((word_count + 1) * sizeof(char *));
+	if (words == NULL) return (NULL);
+
+	i = 0;
+	k = 0;
+	for (j = 0; str[j] != '\0'; j++) {
+		if (str[j] != ' ') {
+			if (j == 0 || str[j - 1] == ' ') {
+				int word_length = 0;
+				int start_index = j;
+
+				while (str[j] != '\0' && str[j] != ' ') {
+					word_length++;
+					j++;
+				}
+
+				words[i] = (char *)malloc((word_length + 1) * sizeof(char));
+				if (words[i] == NULL) {
+					for (int n = 0; n < i; n++)
+						free(words[n]);
+					free(words);
+					return (NULL);
+				}
+
+				strncpy(words[i], &str[start_index], word_length);
+				words[i][word_length] = '\0';
+				i++;
+			}
+		}
+	}
+
+	words[word_count] = NULL;
+
+	return (words);
+}
