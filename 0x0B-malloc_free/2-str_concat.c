89b5efc12f726725fe910993b770c3393604d30c
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-01 02:04:54 +0200

    2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,15 +12,18 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	char *concatenated;
+	int len1, len2;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
-	char *concatenated = malloc((len1 + len2 + 1) * sizeof(char));
+	concatenated = malloc((len1 + len2 + 1) * sizeof(char));
 	if (concatenated == NULL)
 		return NULL;
 
