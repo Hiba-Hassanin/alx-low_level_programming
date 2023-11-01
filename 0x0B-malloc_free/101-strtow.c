dc33a93655a16a849bba4715a6c5fe7afdc1c8c1
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-01 02:46:34 +0200

    sentence to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,36 +9,56 @@
  * Return: NULL if str == NULL or str == ""
 */
 
-char **strtow(char *str) {
-	if (str == NULL || *str == '\0') return NULL;
+#include <stdlib.h>
+#include <string.h>
 
-	int i, j, k, word_count = 0;
+char **strtow(char *str)
+{
+	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
+
+	int i, j, word_count = 0;
 	char **words = NULL;
 
-	for (i = 0; str[i] != '\0'; i++) {
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' ')) word_count++;
+	for (i = 0; str[i] != '\0'; i++){
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			word_count++;
+		}
 	}
 
 	words = (char **)malloc((word_count + 1) * sizeof(char *));
-	if (words == NULL) return (NULL);
+	if (words == NULL)
+	{
+		return (NULL);
+	}
 
+	int n;
 	i = 0;
-	k = 0;
-	for (j = 0; str[j] != '\0'; j++) {
-		if (str[j] != ' ') {
-			if (j == 0 || str[j - 1] == ' ') {
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		if (str[j] != ' ')
+		{
+			if (j == 0 || str[j - 1] == ' ')
+			{
 				int word_length = 0;
 				int start_index = j;
 
-				while (str[j] != '\0' && str[j] != ' ') {
+				while (str[j] != '\0' && str[j] != ' ')
+				{
 					word_length++;
 					j++;
 				}
 
 				words[i] = (char *)malloc((word_length + 1) * sizeof(char));
-				if (words[i] == NULL) {
-					for (int n = 0; n < i; n++)
+				if (words[i] == NULL)
+				{
+					for (n = 0; n < i; n++)
+					{
 						free(words[n]);
+					}
 					free(words);
 					return (NULL);
 				}
