88b1607624fb04ea8755eaa4897c7e656846d83f
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-01 03:07:36 +0200

    sentence to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,31 +1,32 @@
 #include "main.h"
 #include <string.h>
-
 /**
  * **strtow - splits a string into words
  * @str: the string
  * Return: NULL if str == NULL or str == ""
 */
-
-char **strtow(char *str) {
+char **strtow(char *str)
+{
 	int i, j, count = 0;
 	char **words;
 
 	if (!str || !*str)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; str[i]; i++)
 		count += (str[i] != ' ' && (i == 0 || str[i - 1] == ' '));
 
 	if (count == 0)
-		return NULL;
+		return (NULL);
 
 	words = malloc((count + 1) * sizeof(char *));
 	if (!words)
-		return NULL;
+		return (NULL);
 
-	for (i = 0, j = 0; str[j]; j++) {
-		if (str[j] != ' ' && (j == 0 || str[j - 1] == ' ')) {
+	for (i = 0, j = 0; str[j]; j++)
+	{
+		if (str[j] != ' ' && (j == 0 || str[j - 1] == ' '))
+		{
 			int len = strcspn(&str[j], " ");
 			words[i] = malloc((len + 1) * sizeof(char));
 			strncpy(words[i++], &str[j], len);
@@ -34,5 +35,5 @@ char **strtow(char *str) {
 	}
 
 	words[count] = NULL;
-	return words;
+	return (words);
 }
