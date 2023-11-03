ac7bab6c7f02c8c8a92912306bef4518cf7edbbe
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-03 04:32:49 +0200

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,39 +10,31 @@
  * Return: 0
 */
 
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
+int validate_input(char *num) {
+	while (*num) {
+		if (!isdigit(*num))
+			return 0;
+		num++;
+	}
+	return 1;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
 		printf("Error\n");
-		return (98);
+		return 98;
 	}
 
 	char *num1 = argv[1];
 	char *num2 = argv[2];
 
-	while (*num1)
-	{
-		if (!isdigit(*num1))
-		{
-			printf("Error\n");
-			return (98);
-		}
-		num1++;
-	}
-
-	while (*num2)
-	{
-		if (!isdigit(*num2))
-		{
-			printf("Error\n");
-			return (98);
-		}
-		num2++;
+	if (!validate_input(num1) || !validate_input(num2)) {
+		printf("Error\n");
+		return 98;
 	}
 
 	unsigned long long int result = strtoull(argv[1], NULL, 10) * strtoull(argv[2], NULL, 10);
 	printf("%llu\n", result);
 
-	return (0);
+	return 0;
 }
