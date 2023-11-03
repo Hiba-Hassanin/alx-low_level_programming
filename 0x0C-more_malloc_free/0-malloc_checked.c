37032d9595dca4a16dd7d883f7ddfb842cba593c
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-03 03:22:54 +0200

    Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,11 +10,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
-	if (ptr == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
+	int *yu = malloc(b);
+
+	if (yu == 0)
 		exit(98);
-	}
-	return (ptr);
+
+	return (yu);
 }
