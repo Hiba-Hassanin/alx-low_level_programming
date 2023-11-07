11a451cdcf2023448b595313f7568f749ff15b65
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-07 03:15:33 +0200

    Dog info

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,44 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _lenS - show the string length
+ *
+ * @str: the string
+ *
+ * Return: the string length
+*/
+
+int _lenS(const char *str)
+{
+	int sLen = 0;
+
+	while (*str++)
+		sLen++;
+	return (sLen);
+}
+
+/**
+ * _copy - this function copy our string
+ *
+ * @src: first
+ * @dest: second
+ *
+ * Return: @dest
+*/
+
+char *_copy(char *dest, char *src)
+{
+	int co;
+
+	for (co = 0; src[co]; co++)
+		dest[co] = src[co];
+	dest[co] = '\0';
+
+	return (dest);
+
+}
 
 /**
  * init_dog - contain dog info
@@ -13,34 +50,32 @@
  * Return: void
 */
 
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	if (name == NULL || owner == NULL)
-	{
+dog_t *new_dog(char *name, float age, char *owner) {
+	dog_t *dog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (_lenS(name) + 1));
+	if ((*dog).name == NULL) {
+		free(dog);
 		return (NULL);
 	}
 
-	dog_t *new_dog_ptr = malloc(sizeof(dog_t));
-	if (new_dog_ptr == NULL)
-	{
+	dog->owner = malloc(sizeof(char) * (_lenS(owner) + 1));
+	if ((*dog).owner == NULL) {
+		free(dog->owner);
+		free(dog);
 		return (NULL);
 	}
 
-	new_dog_ptr->name = malloc(strlen(name) + 1);
-	new_dog_ptr->owner = malloc(strlen(owner) + 1);
+	dog->name = _copy(dog->name, name);
+	dog->age = age;
+	dog->owner = _copy(dog->owner, owner);
 
-	if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL)
-	{
-		free(new_dog_ptr->name);
-		free(new_dog_ptr->owner);
-		free(new_dog_ptr);
-		return (NULL);
-	}
-
-	strcpy(new_dog_ptr->name, name);
-	strcpy(new_dog_ptr->owner, owner);
-
-	new_dog_ptr->age = age;
-
-	return (new_dog_ptr);
+	return (dog);
 }
