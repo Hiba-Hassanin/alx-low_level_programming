95febf3078ea0d4cb69ce5a0fc1168048fc6f5c1
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-09 19:24:44 +0200

    Sum it all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,19 +10,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
+	int sum = 0, i = n;
+	va_list args;
+
+	if (!n)
 		return (0);
 
-	int sum = 0;
-	va_list args;
 	va_start(args, n);
-
-	for (unsigned int i = 0; i < n; i++)
-	{
+	while (i--)
 		sum += va_arg(args, int);
-	}
-
 	va_end(args);
-
 	return (sum);
 }
