9d487883bb16fd797f4369d647abbd2d44916dc1
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-09 20:15:55 +0200

    pr, pprr, prrr, print all YaaY!

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,43 +6,91 @@
  *
  * Return: void
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
 	char *str;
 
+	token_t tokens[] = {
+			{"c", print_char},
+			{"i", print_int},
+			{"f", print_float},
+			{"s", print_string},
+			{NULL, NULL}
+	};
+
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s", str);
-				break;
-		}
+		unsigned int j = 0;
 
-		if (format[i + 1] && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
-			printf(", ");
+		while (tokens[j].token)
+		{
+			if (*tokens[j].token == format[i])
+			{
+				tokens[j].f(args);
+				if (format[i + 1])
+					printf(", ");
+				break;
+			}
+			j++;
+		}
 
 		i++;
 	}
 
-	printf("\n");
-
 	va_end(args);
+
+	printf("\n");
+}
+
+/**
+ * print_char - Prints a character.
+ * @args: The va_list containing the character argument.
+ *
+ * Return: void
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - Prints an integer.
+ * @args: The va_list containing the integer argument.
+ *
+ * Return: void
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - Prints a float.
+ * @args: The va_list containing the float argument.
+ *
+ * Return: void
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - Prints a string.
+ * @args: The va_list containing the string argument.
+ *
+ * Return: void
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+
+	printf("%s", str);
 }
