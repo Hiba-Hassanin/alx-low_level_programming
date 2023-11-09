1894a1e594ee4fb768df093bb299106782bf7990
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-09 02:27:30 +0200

    array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,11 @@
  * Return: void
 */
 
-void array_iterator(int *array, unsigned int size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-	for (i = 0; i < size; i++)
-	{
-		action(array[i]);
-	}
+	int *end = array + size - 1;
+
+	if (array && size && action)
+		while (array <= end)
+			action(*array++);
 }
