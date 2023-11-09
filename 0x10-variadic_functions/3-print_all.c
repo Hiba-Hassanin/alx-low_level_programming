c52a765bce2261b890174c8747672ad0da438497
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-09 20:40:33 +0200

    pr, pprr, prrr, print all YaaY!

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,56 +1,61 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_char - Prints a character.
+ * @separator: The separator string.
  * @ap: The va_list containing the character argument.
- * @separtor: the separator
  *
  * Return: void
  */
-void print_char(char *separtor, va_list ap)
+void print_char(char *separator, va_list ap)
 {
-	printf("%s%c", separtor, va_arg(ap, int));
+	printf("%s%c", separator, va_arg(ap, int));
 }
 
 /**
  * print_int - Prints an integer.
+ * @separator: The separator string.
  * @ap: The va_list containing the integer argument.
- * @separtor: the separator
  *
  * Return: void
  */
-void print_int(char *separtor, va_list ap)
+void print_int(char *separator, va_list ap)
 {
-	printf("%s%d", separtor, va_arg(ap, int));
+	printf("%s%d", separator, va_arg(ap, int));
 }
 
 /**
  * print_float - Prints a float.
+ * @separator: The separator string.
  * @ap: The va_list containing the float argument.
- * @separtor: the separator
+ *
  * Return: void
  */
-void print_float(char *separtor, va_list ap)
+void print_float(char *separator, va_list ap)
 {
-	printf("%s%f", separtor, va_arg(ap, double));
+	printf("%s%f", separator, va_arg(ap, double));
 }
 
 /**
  * print_string - Prints a string.
+ * @separator: The separator string.
  * @ap: The va_list containing the string argument.
- * @separtor: the separator
  *
  * Return: void
  */
-void print_string(char *separtor, va_list ap)
+void print_string(char *separator, va_list ap)
 {
 	char *str = va_arg(ap, char *);
 
 	switch ((int)(!str))
+	{
 		case 1:
 			str = "(nil)";
+	}
 
-	printf("%s%s", separtor, str);
+	printf("%s%s", separator, str);
 }
 
 /**
@@ -86,6 +91,7 @@ void print_all(const char * const format, ...)
 			{
 				tokens[j].f(separator, ap);
 				separator = ", ";
+				break;
 			}
 			j++;
 		}
