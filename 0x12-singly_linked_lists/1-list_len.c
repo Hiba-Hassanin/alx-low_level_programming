19e9459ce8787ac041c60c8513558aad4d040a43
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-17 00:12:03 +0200

    list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,6 +6,7 @@
  *
  * Return: The number of elements in the list.
  */
+
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
@@ -16,5 +17,5 @@ size_t list_len(const list_t *h)
 		h = h->next;
 	}
 
-	return count;
+	return (count);
 }
