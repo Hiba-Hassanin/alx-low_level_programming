9f957a853d3f42dcf247416540ec799ece1b3cfd
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-17 00:57:46 +0200

    list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,12 @@
-section .data
-    hello db "Hello, Holberton", 0
-    len equ $ - hello
+section          .text
+     extern      printf
+     global      main
 
-section .text
-    global _start
+main:
+     mov                  edi, msg
+     mov                  eax, 0
+     call        printf
 
-_start:
-    ; Prepare arguments for the write system call
-    mov rax, 1             ; System call number for write
-    mov rdi, 1             ; File descriptor 1 (stdout)
-    mov rsi, hello         ; Address of the message string
-    mov rdx, len           ; Length of the message
-    syscall
 
-    ; Exit the program
-    xor edi, edi
-    mov eax, 60
-    syscall
+section          .data
+       msg db 'Hello, Holberton', 0xa, 0
