fbe5d91c033985f3e91fb5bee42b79014c769c54
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-21 22:43:22 +0200

    inserts a new node

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node in listint_t
+ * @head: Pointer to a pointer
+ * @idx: Index
+ * @n: Value to be stored
+ *
+ * Return: The address of the new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *current;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	current = *head;
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (current == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
+}
