02ae14bd531e1478fd127282acbc55e5946ef5d3
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-28 04:36:59 +0200

    copies content

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,89 +1,68 @@
-#include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 
-void handle_error(int code, const char *file)
+#define READ_BUF_SIZE 1024
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %s\n"
+
+int main(int argc, char **argv)
 {
-	switch (code)
+	int fd_from = 0, fd_to = 0;
+	ssize_t bytes_r_w;
+	char buffer[READ_BUF_SIZE];
+
+	if (argc != 3)
 	{
-		case 97:
-			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-			break;
-		case 98:
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-			break;
-		case 99:
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-			break;
-		case 100:
-			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file);
-			break;
-		default:
-			break;
+		dprintf(STDERR_FILENO, USAGE);
+		exit(97);
 	}
 
-	exit(code);
-}
-
-int copy_file(const char *file_from, const char *file_to)
-{
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUF_SIZE];
-
-	/* Open the source file */
-	fd_from = open(file_from, O_RDONLY);
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		handle_error(98, file_from);
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]);
+		exit(98);
 	}
 
-	/* Open the destination file */
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (fd_to == -1)
 	{
-		handle_error(99, file_to);
+		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]);
+		exit(99);
 	}
 
-	/* Copy the contents from source to destination */
-	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
+	while ((bytes_r_w = read(fd_from, buffer, READ_BUF_SIZE)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
+		if (write(fd_to, buffer, bytes_r_w) != bytes_r_w)
 		{
-			handle_error(99, file_to);
+			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]);
+			exit(99);
 		}
 	}
 
-	if (bytes_read == -1)
+	if (bytes_r_w == -1)
 	{
-		handle_error(98, file_from);
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]);
+		exit(98);
 	}
 
-	/* Close file descriptors */
 	if (close(fd_from) == -1)
 	{
-		handle_error(100, file_from);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, argv[1]);
+		exit(100);
 	}
+
 	if (close(fd_to) == -1)
 	{
-		handle_error(100, file_to);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, argv[2]);
+		exit(100);
 	}
 
-	return 0;
-}
-
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		handle_error(97, NULL);
-	}
-
-	copy_file(argv[1], argv[2]);
-
-	return 0;
+	return EXIT_SUCCESS;
 }
