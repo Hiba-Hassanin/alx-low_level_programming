0cd38a8409ac1bbfc7bc85a691270974280edb00
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-28 02:47:40 +0200

    To The POSIX

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,27 +14,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_r_w;
 	char buffer[READ_BUF_SIZE * 8];
 
-	if (filename == NULL)
+	if (!filename || !letters)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
-	bytes_r_w = read(fd, buffer, letters);
-	if (bytes_r_w == -1)
-	{
-		close(fd);
-		return (0);
-	}
-
-	bytes_r_w = write(STDOUT_FILENO, buffer, bytes_r_w);
-	if (bytes_r_w == -1 || (size_t)bytes_r_w != (size_t)letters)
-	{
-		close(fd);
-		return (0);
-	}
-
+	bytes_r_w = read(fd, &buffer[0], letters);
+	bytes_r_w = write(STDOUT_FILENO, &buffer[0], bytes_r_w);
 	close(fd);
 	return (bytes_r_w);
 }
