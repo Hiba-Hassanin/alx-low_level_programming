dd0fa9e6d9bdcc5864e1d8e721fe82b0ec1c34c6
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-28 04:59:53 +0200

    copies content

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,8 +11,7 @@
  * print_error - Prints an error message to stderr and exits.
  * @message: The error message to print.
  */
-void print_error(const char *message)
-{
+void print_error(const char *message) {
 	fprintf(stderr, "%s", message);
 	exit(98);
 }
@@ -21,19 +20,15 @@ void print_error(const char *message)
  * print_elf_header - Prints the information contained in the ELF header.
  * @header: A pointer to the ELF header structure.
  */
-void print_elf_header(const Elf64_Ehdr *header)
-{
-	int i;
-
-	printf("ELF Header:\n");
-	printf("  Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
+void print_elf_header(const Elf64_Ehdr *header) {
+	printf("Magic:   ");
+	for (int i = 0; i < EI_NIDENT; i++) {
 		printf("%02x ", header->e_ident[i]);
+	}
 	printf("\n");
 
-	printf("  Class:                             ");
-	switch (header->e_ident[EI_CLASS])
-	{
+	printf("Class:                             ");
+	switch (header->e_ident[EI_CLASS]) {
 		case ELFCLASS32:
 			printf("ELF32\n");
 			break;
@@ -45,9 +40,8 @@ void print_elf_header(const Elf64_Ehdr *header)
 			break;
 	}
 
-	printf("  Data:                              ");
-	switch (header->e_ident[EI_DATA])
-	{
+	printf("Data:                              ");
+	switch (header->e_ident[EI_DATA]) {
 		case ELFDATA2LSB:
 			printf("2's complement, little endian\n");
 			break;
@@ -59,12 +53,10 @@ void print_elf_header(const Elf64_Ehdr *header)
 			break;
 	}
 
-	printf("  Version:                           %d (current)\n",
-		   header->e_ident[EI_VERSION]);
+	printf("Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
 
-	printf("  OS/ABI:                            ");
-	switch (header->e_ident[EI_OSABI])
-	{
+	printf("OS/ABI:                            ");
+	switch (header->e_ident[EI_OSABI]) {
 		case ELFOSABI_SYSV:
 			printf("UNIX - System V\n");
 			break;
@@ -79,12 +71,10 @@ void print_elf_header(const Elf64_Ehdr *header)
 			break;
 	}
 
-	printf("  ABI Version:                       %d\n",
-		   header->e_ident[EI_ABIVERSION]);
+	printf("ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
 
-	printf("  Type:                              ");
-	switch (header->e_type)
-	{
+	printf("Type:                              ");
+	switch (header->e_type) {
 		case ET_NONE:
 			printf("NONE (Unknown type)\n");
 			break;
@@ -105,8 +95,7 @@ void print_elf_header(const Elf64_Ehdr *header)
 			break;
 	}
 
-	printf("  Entry point address:               0x%lx\n",
-		   header->e_entry);
+	printf("Entry point address:               %lx\n", header->e_entry);
 }
 
 /**
@@ -116,32 +105,32 @@ void print_elf_header(const Elf64_Ehdr *header)
  *
  * Return: 0 on success, 98 on error.
  */
-int main(int argc, char **argv)
-{
-	int fd;
-	Elf64_Ehdr header;
-	ssize_t bytes_read;
-
-	if (argc != 2)
+int main(int argc, char **argv) {
+	if (argc != 2) {
 		print_error(USAGE);
+	}
 
 	const char *filename = argv[1];
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1) {
 		print_error("Error: Could not open file.\n");
+	}
 
-	bytes_read = read(fd, &header, sizeof(header));
-	if (bytes_read == -1 || bytes_read != sizeof(header))
+	Elf64_Ehdr header;
+	ssize_t bytes_read = read(fd, &header, sizeof(header));
+	if (bytes_read == -1 || bytes_read != sizeof(header)) {
 		print_error("Error: Failed to read ELF header.\n");
+	}
 
-	/* Check the ELF magic number */
-	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
+	// Check the ELF magic number
+	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0) {
 		print_error("Error: Not an ELF file.\n");
+	}
 
 	print_elf_header(&header);
 
 	close(fd);
 
-	return (0);
+	return 0;
 }
