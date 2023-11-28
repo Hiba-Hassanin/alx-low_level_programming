a9cdb82d7f3876a4ec81a4da935ff3e423e31390
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-28 04:33:06 +0200

    copies content

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,33 +21,61 @@
  * Return: 0 if successful
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, USAGE);
+		exit(97);
+	}
+
 	int fd_from = 0, fd_to = 0;
 	ssize_t bytes_r_w;
 	char buffer[READ_BUF_SIZE];
 
-	if (argc != 3)
-		dprintf(STDERR_FILENO, USAGE), exit(97);
+	/* Open the source file */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]);
+		exit(98);
+	}
+
+	/* Open the destination file */
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+	{
+		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]);
+		exit(99);
+	}
 
+	/* Copy the contents from source to destination */
 	while ((bytes_r_w = read(fd_from, buffer, READ_BUF_SIZE)) > 0)
+	{
 		if (write(fd_to, buffer, bytes_r_w) != bytes_r_w)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+		{
+			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]);
+			exit(99);
+		}
+	}
+
 	if (bytes_r_w == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]);
+		exit(98);
+	}
 
-	fd_from = close(fd_from);
-	fd_to = close(fd_to);
-	if (fd_from)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
-	if (fd_to)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
+	/* Close file descriptors */
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, argv[1]);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, argv[2]);
+		exit(100);
+	}
 
-	return (EXIT_SUCCESS);
+	return 0;
 }
