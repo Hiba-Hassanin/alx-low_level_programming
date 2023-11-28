19441f0467c0b017e7f8ab8d25ec3e030b2d2529
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2023-11-28 03:50:18 +0200

    copies content

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,15 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 
 /**
- * handle_error - Handles the error message
- * @code: The error code
- * @file: The name of the file
+ * handle_error - Handles and displays the appropriate error message.
+ * @code: The error code.
+ * @file: The name of the file.
  */
-
 void handle_error(int code, const char *file)
 {
 	switch (code)
@@ -17,7 +21,7 @@ void handle_error(int code, const char *file)
 			fprintf(stderr, "Error: Can't read from file %s\n", file);
 			break;
 		case 99:
-			fprintf(stderr, "Error: Can't write to %s\n", file);
+			fprintf(stderr, "Error: Can't write to file %s\n", file);
 			break;
 		case 100:
 			fprintf(stderr, "Error: Can't close fd %s\n", file);
@@ -30,31 +34,33 @@ void handle_error(int code, const char *file)
 }
 
 /**
- * copy_file - Copies the content of a file to another file
- * @file_from: The name of the first file
- * @file_to: The name of the second file
+ * copy_file - Copies the content of a file to another file.
+ * @file_from: The name of the source file.
+ * @file_to: The name of the destination file.
  *
- * Return: 0 on success
+ * Return: 0 on success, or exits with the appropriate error code on failure.
  */
-
 int copy_file(const char *file_from, const char *file_to)
 {
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
 	char buffer[BUF_SIZE];
 
+	/* Open the source file */
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
 		handle_error(98, file_from);
 	}
 
+	/* Open the destination file */
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd_to == -1)
 	{
 		handle_error(99, file_to);
 	}
 
+	/* Copy the contents from source to destination */
 	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
@@ -69,6 +75,7 @@ int copy_file(const char *file_from, const char *file_to)
 		handle_error(98, file_from);
 	}
 
+	/* Close file descriptors */
 	if (close(fd_from) == -1)
 	{
 		handle_error(100, file_from);
@@ -82,13 +89,12 @@ int copy_file(const char *file_from, const char *file_to)
 }
 
 /**
- * main - Entry point
- * @argc: The number of arguments
- * @argv: An array of argument
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
  *
- * Return: Always 0
+ * Return: 0 on success.
  */
-
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
