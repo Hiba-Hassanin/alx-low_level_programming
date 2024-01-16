7ff606000e1b43784acd87097c0e49d67fe7842d
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-16 04:01:44 +0200

    c Library

--- 0x18-dynamic_libraries/_memcpy.c
+++ 0x18-dynamic_libraries/_memcpy.c
@@ -2,29 +2,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *ptr = dest;
-	while (n > 0)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
-	}
-	return ptr`_strchr.c`:
-	```c
+	unsigned int i;
 
-#include "main.h"
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
-	char *_strchr(char *s, char c)
-	{
-		while (*s != '\0')
-		{
-			if (*s == c)
-				return s;
-			s++;
-		}
-		if (*s == c)
-			return s;
-		return NULL;
-	}
+	return dest;
 }
