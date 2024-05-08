f81f3287c2eecc090af036d0669541e891c7ef66
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-05-08 21:35:43 +0200

    Search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,45 +1,70 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array
- * of integers using binary search algorithm
+ * recursive_binary_search - Recursively searches for a value in a sorted array
+ * @arr: Pointer to the first element of the array to search in
+ * @left: Index of the leftmost element of the subarray
+ * @right: Index of the rightmost element of the subarray
+ * @val: Value to search for
  *
- * @array: Pointer to the first element of array to search
- * @size: Number of elements in the array
- * @value: Value to search for
- *
- * Return: The index where value is located,
- * or -1 if value is not present or if array is NULL
+ * Return: Index where value is located, or -1 if not found
  */
 
-int binary_search(int *array, size_t size, int value)
+int recursive_binary_search(int *arr, int left, int right, int val)
 {
-	size_t left = 0, right = size - 1, mid, i;
-
-	if (array == NULL)
-		return -1;
+	int mid;
 
 	while (left <= right)
 	{
-		printf("Searching in array: ");
-		for (i = left; i <= right; i++)
-		{
-			printf("%d", array[i]);
-			if (i < right)
-				printf(", ");
-			else
-				printf("\n");
-		}
+		mid = left + (right - left) / 2;
+		print_array(arr, left, right);
 
-		mid = (left + right) / 2;
-
-		if (array[mid] == value)
+		if (arr[mid] == val)
 			return mid;
-		else if (array[mid] < value)
+		else if (arr[mid] < val)
 			left = mid + 1;
 		else
 			right = mid - 1;
 	}
 
-	return -1; // If the value not found
+	return -1;
+}
+
+/**
+ * print_array - Prints the subarray being searched
+ * @arr: Pointer to the first element of the array to print
+ * @start: Index of the start of the subarray
+ * @end: Index of the end of the subarray
+ *
+ * Return: void
+ */
+
+void print_array(int *arr, int start, int end)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		printf("%d", arr[i]);
+		if (i < end)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - Searches for a value in a sorted array using binary search
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index where value is located, or -1 if not found or array is NULL
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+		return -1;
+
+	return recursive_binary_search(array, 0, (int)size - 1, value);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int recursive_binary_search(int *arr, int left, int right, int val);
+void print_array(int *arr, int start, int end);
 
 #endif /* SEARCH_ALGOS_H */
