977d73baaa8952babd0ebfdce0aada1227618665
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-05-08 23:09:33 +0200

    Search

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -8,6 +8,7 @@
  * Return: Pointer to the first node where value is located, or NULL if not
  * found or if head is NULL.
  */
+
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *current, *express;
