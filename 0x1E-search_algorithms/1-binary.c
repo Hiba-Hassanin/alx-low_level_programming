d2cc71419f50ed46eaf0e8434f680463bdb7bb65
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-05-08 21:40:30 +0200

    Search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,13 +1,16 @@
 #include "search_algos.h"
 
 /**
- * recursive_binary_search - Recursively searches for a value in a sorted array
- * @arr: Pointer to the first element of the array to search in
- * @left: Index of the leftmost element of the subarray
- * @right: Index of the rightmost element of the subarray
+ * recursive_binary_search - Recursively searches
+ * for a value in a sorted array
+ *
+ * @arr: Pointer to 1st element of array to search in
+ * @left: Index of the leftmost element of subarray
+ * @right: Index of the rightmost element of subarray
  * @val: Value to search for
  *
- * Return: Index where value is located, or -1 if not found
+ * Return: Index where value is located,
+ * or -1 if not found
  */
 
 int recursive_binary_search(int *arr, int left, int right, int val)
@@ -32,7 +35,8 @@ int recursive_binary_search(int *arr, int left, int right, int val)
 
 /**
  * print_array - Prints the subarray being searched
- * @arr: Pointer to the first element of the array to print
+ *
+ * @arr: Pointer to the 1st element of array to print
  * @start: Index of the start of the subarray
  * @end: Index of the end of the subarray
  *
@@ -54,17 +58,21 @@ void print_array(int *arr, int start, int end)
 }
 
 /**
- * binary_search - Searches for a value in a sorted array using binary search
- * @array: Pointer to the first element of the array to search in
+ * binary_search - Searches for a value
+ * in a sorted array using binary search
+ *
+ * @array: Pointer to 1st element array to search in
  * @size: Number of elements in the array
  * @value: Value to search for
  *
- * Return: Index where value is located, or -1 if not found or array is NULL
+ * Return: Index where value is located,
+ * or -1 if not found or array is NULL
  */
+
 int binary_search(int *array, size_t size, int value)
 {
 	if (array == NULL || size == 0)
 		return -1;
 
-	return recursive_binary_search(array, 0, (int)size - 1, value);
+	return recursive_binary_search(array,0,(int)size - 1, value);
 }
