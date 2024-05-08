0fce6957fbbce137e0f36e60f7f745d6b7b114dc
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-05-08 23:08:30 +0200

    Search

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - Searches for a value in a sorted skip list of integers.
+ * @list: Pointer to the head of the skip list to search in.
+ * @value: The value to search for.
+ *
+ * Return: Pointer to the first node where value is located, or NULL if not
+ * found or if head is NULL.
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *current, *express;
+
+	if (list == NULL)
+		return (NULL);
+
+	current = list;
+	express = list->express;
+
+	while (express)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
+		if (express->n >= value || express->express == NULL)
+		{
+			printf("Value found between indexes [%lu] and [%lu]\n",
+				   current->index, express->index);
+			break;
+		}
+		current = express;
+		express = express->express;
+	}
+
+	if (!express)
+	{
+		while (current->next)
+			current = current->next;
+		printf("Value found between indexes [%lu] and [%lu]\n",
+			   current->index, current->index);
+	}
+
+	while (current != NULL && current->index <= express->index)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+		if (current->n == value)
+			return (current);
+		current = current->next;
+	}
+
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -14,12 +15,32 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct listint_s
 {
 	int n;
 	size_t index;
 	struct listint_s *next;
 } listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
 
 
 int linear_search(int *array, size_t size, int value);
